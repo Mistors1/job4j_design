bc9c6258a30eb9201ed75836840a7864cec0cb7d
Mistors1 <Kolov-dm@bk.ru>
2023-03-09 16:39:37 +0300

    Container

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -9,7 +9,7 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public void add(T model) {
-      storage.putIfAbsent(model.getId(), model);
+        storage.putIfAbsent(model.getId(), model);
     }
 
     @Override

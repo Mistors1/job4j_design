519bc4b339d428098b9ab2d330365ed04aaff537
Mistors1 <Kolov-dm@bk.ru>
2023-03-13 14:38:24 +0300

    ForwardLinked

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,91 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<E> implements Iterable<E> {
+
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        Node<E> currentNode = head;
+        if (head == null) {
+            head = newNode;
+            modCount++;
+            size++;
+        } else {
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+            modCount++;
+            size++;
+        }
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node.item;
+    }
+
+    public E deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<E> node = head;
+            head = null;
+            size--;
+            for (int i = 0; i < size; i++) {
+                head = node.next;
+        }
+        return node.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            int valueIt = 0;
+            final int expectedModCount = modCount;
+
+            Node<E> startNode = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return valueIt < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                valueIt++;
+                Node<E> rsl = startNode;
+                startNode = startNode.next;
+                return rsl.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private final E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}

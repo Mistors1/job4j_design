710b89437e80cd6fb664afa5c5ca26aa66c9a2f6
Mistors1 <Kolov-dm@bk.ru>
2023-03-20 17:36:35 +0300

    SimpleTree

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,48 @@
+package ru.job4j.tree;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> parents = findBy(parent);
+        if (parents.isPresent() && findBy(child).isEmpty()) {
+            parents.get().children.add(new Node<>(child));
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+      return findByPredicate(eValue -> eValue.value.equals(value));
+    }
+
+    public boolean isBinary() {
+        return findByPredicate(value -> value.children.size() > 2).isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (condition.test(el)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}

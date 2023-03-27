5ec3730d9e9c29a350eced7b4b05df9d15b7ef83
Mistors1 <Kolov-dm@bk.ru>
2023-03-27 11:19:06 +0300

    BufferedOutput

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -7,18 +7,29 @@ public class LogFilter {
     public List<String> filter(String file) {
         List<String> buffered = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-           buffered.add(in.lines().filter(s -> s.contains(" 404 "))
-                   .toList().toString());
+           buffered.addAll(in.lines().filter(s -> s.contains(" 404 "))
+                   .toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return buffered;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)))) {
+            out.println(String.join(System.lineSeparator(), log));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("data/log");
-        System.out.println(log);
+        save(log, "data/404");
+        System.out.println(String.join(System.lineSeparator(), log));
 
     }
 }

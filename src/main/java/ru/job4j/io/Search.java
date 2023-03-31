832782686b2e4de60715803df04682f5cc555931
Mistors1 <Kolov-dm@bk.ru>
2023-03-31 13:02:58 +0300

    SearchValidation

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,24 @@ import java.util.function.*;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        Search search = new Search();
+        search.validate(args);
+        Path start = Paths.get(args[0]);
+        String word = args[1];
+        search(start, p -> p.toFile().getName().endsWith(word)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPath();
+    }
+
+    public void validate(String[] args) {
+        if (args.length != 2
+                || args[0] == null
+                || args[1] == null) {
+            throw new IllegalArgumentException("Incorrect arguments");
+        }
     }
 }

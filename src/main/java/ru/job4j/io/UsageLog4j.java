2275f28059fa7a0a50ca0b0c0c0225a0fd6bb851
Mistors1 <Kolov-dm@bk.ru>
2023-04-13 15:20:39 +0300

    Logs

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,4 +1,5 @@
 package ru.job4j.io;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,10 +8,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        double d = 13.2;
+        float f = 11f;
+        long l = 1111;
+        byte b = 1;
+        boolean bo = true;
+        char c = 'a';
+        short s = 222;
+        LOG.debug("User info name : {}, age : {}, {},{} ,{} ,{} ,{} ,{} ,{} ", name, age, d, f, l, b, bo, c, s);
     }
 }
